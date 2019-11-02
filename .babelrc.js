fb67a24db8446828243b1a862da7da3ae025aace
Dan Train <dan.train@red-badger.com>
2019-11-02 18:41:29 +0000

    Update babel settings

---
+++ .babelrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+        modules: false,
+        useBuiltIns: 'usage',
+        shippedProposals: true,
+        corejs: { version: 2 },
+      },
+    ],
+    ['@babel/preset-react', { useBuiltIns: true }],
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-syntax-dynamic-import',
+    'babel-plugin-macros',
+    ['@babel/plugin-transform-runtime', { helpers: true, regenerator: true }],
+  ],
+};
