fd092cd9934e4b908c557130c1d2124401451a11
Dan Train <dan.train@red-badger.com>
2019-11-04 21:25:03 +0000

    Clean up gatsby-node script

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,70 +1,49 @@
 const path = require('path');
-const fs = require('fs');
 const times = require('lodash/times');
 
-const createPaginationJSON = (pathSuffix, pagePosts) => {
-  const dir = 'public/paginationJson/';
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
-  const filePath = dir + 'index' + pathSuffix + '.json';
-  const dataToSave = JSON.stringify(pagePosts);
-  fs.writeFile(filePath, dataToSave, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-  });
-};
-
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(`
-        {
-          allContentfulPodcastPost {
-            edges {
-              node {
-                slug
-              }
-            }
+exports.createPages = ({ graphql, actions: { createPage } }) =>
+  graphql(`
+    {
+      allContentfulPodcastPost {
+        edges {
+          node {
+            slug
           }
         }
-      `).then(result => {
-        if (result.errors) {
-          console.log(result.errors);
-          reject(result.errors);
-        }
+      }
+    }
+  `).then(
+    ({
+      errors,
+      data: {
+        allContentfulPodcastPost: { edges: posts },
+      },
+    }) => {
+      if (errors) {
+        console.log(errors);
+        throw errors;
+      }
 
-        const posts = result.data.allContentfulPodcastPost.edges;
-
-        posts.forEach(post => {
-          createPage({
-            path: `/${post.node.slug}/`,
-            component: path.resolve(
-              './src/templates/PodcastPost/PodcastPost.js'
-            ),
-            context: {
-              slug: post.node.slug,
-            },
-          });
+      posts.forEach(({ node: { slug } }) => {
+        createPage({
+          path: `/${slug}/`,
+          component: path.resolve('./src/templates/PodcastPost/PodcastPost.js'),
+          context: { slug },
         });
+      });
 
-        const postsPerPage = 5;
-        const numPages = Math.ceil(posts.length / postsPerPage);
+      const postsPerPage = 5;
+      const numPages = Math.ceil(posts.length / postsPerPage);
 
-        times(numPages, i => {
-          createPage({
-            path: `/${i > 0 ? i + 1 : ''}`,
-            component: path.resolve('./src/templates/Home/Home.js'),
-            context: {
-              limit: postsPerPage,
-              skip: i * postsPerPage,
-            },
-          });
+      times(numPages, i => {
+        createPage({
+          path: `/${i > 0 ? i + 1 : ''}`,
+          component: path.resolve('./src/templates/Home/Home.js'),
+          context: {
+            limit: postsPerPage,
+            skip: i * postsPerPage,
+          },
         });
-      })
-    );
-  });
-};
+      });
+    }
+  );
