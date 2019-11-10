dc9442ea79bf091fa38c6c0718d3b443ae6e643d
Dan Train <dan.train@red-badger.com>
2019-11-10 03:00:27 +0000

    Add Post interface

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,77 @@
 const path = require('path');
 const times = require('lodash/times');
 
+// const postTypes = ['ContentfulPodcastPost', 'ContentfulVideoPost'];
+
+// exports.createSchemaCustomization = ({ actions: { createTypes }, schema }) =>
+//   createTypes([
+//     schema.buildUnionType({
+//       name: 'Post',
+//       types: postTypes,
+//       resolveType: node => node.internal.type,
+//     }),
+//   ]);
+
+exports.createSchemaCustomization = ({ actions: { createTypes } }) =>
+  createTypes(`
+      interface Post @nodeInterface {
+        id: ID!
+        slug: String
+        recordingDate: Date
+      }
+      type ContentfulPodcastPost implements Node & Post @infer {
+        id: ID!
+      }
+      type ContentfulVideoPost implements Node & Post @infer {
+        id: ID!
+      }
+  `);
+
+// exports.createResolvers = ({ createResolvers }) =>
+//   createResolvers({
+//     Query: {
+//       allPost: {
+//         type: `[Post!]!`,
+//         resolve(source, args, context, info) {
+//           return postTypes
+//             .reduce(
+//               (posts, type) => [
+//                 ...posts,
+//                 ...context.nodeModel.getAllNodes({ type }),
+//               ],
+//               []
+//             )
+//             .sort((p1, p2) =>
+//               new Date(p1.recordingDate) > new Date(p2.recordingDate) ? -1 : 1
+//             );
+//         },
+//       },
+//     },
+//   });
+
 exports.createPages = ({ graphql, actions: { createPage } }) =>
   graphql(`
     {
-      allContentfulVideoPost {
+      allPost(sort: { fields: [recordingDate], order: DESC }) {
         edges {
           node {
+            __typename
             slug
           }
         }
       }
     }
-  `).then(({ errors, data: { allContentfulVideoPost: { edges: posts } } }) => {
+  `).then(({ errors, data: { allPost: { edges: posts } } }) => {
     if (errors) {
       console.log(errors);
       throw errors;
     }
 
-    posts.forEach(({ node: { slug } }) => {
+    posts.forEach(({ node: { __typename, slug } }) => {
+      const type = __typename.replace('Contentful', '');
       createPage({
         path: `/${slug}/`,
-        component: path.resolve('./src/templates/VideoPost/VideoPost.js'),
+        component: path.resolve(`./src/templates/${type}/${type}.js`),
         context: { slug },
       });
     });
