97e0694b78d5cbe529f64c859e52ef5d4d32c5d6
Dan Train <dan.train@red-badger.com>
2019-11-30 23:34:19 +0000

    Start of mobile menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,38 +1,44 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'gatsby';
+import cn from 'classnames';
 import GamesList from '../GamesList';
 
 import s from './layout.module.css';
 
-const Layout = ({ children, hotTopics }) => (
-  <div className={s.layout}>
-    <main className={s.main}>{children}</main>
-    <section className={s.sideBarContainer}>
-      <div className={s.sideBar}>
-        <nav>
-          <ul>
-            <li>
-              <Link className={s.navLink} to="/saladcast">
-                Saladcast
-              </Link>
-            </li>
-            <li>
-              <Link className={s.navLink} to="/video-thing">
-                Video Thing
-              </Link>
-            </li>
-            <li>
-              <Link className={s.navLink} to="/about">
-                About
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <h3 className={s.hotTopicsTitle}>Hot Topics</h3>
-        <GamesList games={hotTopics} />
-      </div>
-    </section>
-  </div>
-);
+const Layout = ({ children, hotTopics }) => {
+  const { url, title } = useSelector(state => state.player);
+
+  return (
+    <div className={s.layout}>
+      <main className={s.main}>{children}</main>
+      <section className={s.sideBarContainer}>
+        <div className={cn(s.sideBar, { [s.playerOpen]: url && title })}>
+          <nav>
+            <ul>
+              <li>
+                <Link className={s.navLink} to="/saladcast">
+                  Saladcast
+                </Link>
+              </li>
+              <li>
+                <Link className={s.navLink} to="/video-thing">
+                  Video Thing
+                </Link>
+              </li>
+              <li>
+                <Link className={s.navLink} to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <h3 className={s.hotTopicsTitle}>Hot Topics</h3>
+          <GamesList games={hotTopics} />
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Layout;
