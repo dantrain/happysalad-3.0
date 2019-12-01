d35d9ae56c904dad9c2f31e4d4e83b18c6cc214b
Dan Train <dan.train@red-badger.com>
2019-12-01 22:54:16 +0000

    Fix search ssr

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -8,7 +8,9 @@ import Vh from '../VisuallyHidden';
 
 import s from './search.module.css';
 
-const onMobile = window.matchMedia('(max-width: 600px)').matches;
+const onMobile =
+  typeof window !== 'undefined' &&
+  window.matchMedia('(max-width: 600px)').matches;
 
 const SearchIcon = props => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" {...props}>
