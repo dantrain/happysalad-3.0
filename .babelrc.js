3050f2a841a4d9dbc76c8357327a76bb67f3dabd
Dan Train <dan.train@red-badger.com>
2020-01-03 17:22:46 +0000

    Simplify babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -14,9 +14,6 @@ module.exports = {
   ],
   plugins: [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-syntax-dynamic-import',
-    'babel-plugin-macros',
-    ['@babel/plugin-transform-runtime', { helpers: true, regenerator: true }],
   ],
 };
