a9f52ca2a0c1ed5fda7e6363d5b24af0090548da
Dan Train <dan.train@red-badger.com>
2020-01-09 20:57:44 +0000

    Type Player

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -6,6 +6,7 @@ import { CSSTransition } from 'react-transition-group';
 import { PlayerContextProvider } from '@cassette/core';
 import { MediaProgress } from '@cassette/player';
 import { usePlayerContext } from '@cassette/hooks';
+import { RootState } from '../../store';
 import Container from '../Container/Container';
 import Vh from '../VisuallyHidden/VisuallyHidden';
 import VolumeControl from '../VolumeControl/VolumeControl';
@@ -21,12 +22,15 @@ import { togglePlay, close } from '../../features/player/playerSlice';
 import './cassette-player.css';
 import s from './player.module.css';
 
-const Button = ({ className, ...rest }) => (
-  <button className={cn(s.button, className)} type="button" {...rest} />
-);
+const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
+  className,
+  ...rest
+}) => <button className={cn(s.button, className)} type="button" {...rest} />;
 
-const MediaPlayer = () => {
-  const { playing: shouldPlay } = useSelector(state => state.player);
+const MediaPlayer: React.FC = () => {
+  const { playing: shouldPlay } = useSelector(
+    (state: RootState) => state.player
+  );
   const {
     paused,
     onTogglePause,
@@ -59,12 +63,16 @@ const MediaPlayer = () => {
     return () => clearTimeout(timeout);
   }, [shouldPlay, paused]);
 
-  const onTogglePlay = () => dispatch(togglePlay());
-  const onClose = () => dispatch(close());
-  const onSkipForward = () => {
+  const onTogglePlay = (): ReturnType<typeof togglePlay> =>
+    dispatch(togglePlay());
+
+  const onClose = (): ReturnType<typeof close> => dispatch(close());
+
+  const onSkipForward = (): void => {
     onSeekComplete(clamp(currentTime + 10, 0, duration));
   };
-  const onSkipBackward = () => {
+
+  const onSkipBackward = (): void => {
     onSeekComplete(clamp(currentTime - 10, 0, duration));
   };
 
@@ -109,8 +117,8 @@ const MediaPlayer = () => {
   );
 };
 
-const Player = () => {
-  const { url, title } = useSelector(state => state.player);
+const Player: React.FC = () => {
+  const { url, title } = useSelector((state: RootState) => state.player);
   const playlist = url && title ? [{ url, title }] : [];
 
   return (
