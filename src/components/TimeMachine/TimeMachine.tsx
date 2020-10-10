caa469a3d1b7f892ee2bf6aeeb8bb17233c0a617
Dan Train <dan.train@red-badger.com>
2020-10-11 00:50:53 +0100

    Flesh out TimeMachine component

--- src/components/TimeMachine/TimeMachine.tsx
+++ src/components/TimeMachine/TimeMachine.tsx
@@ -1,16 +1,44 @@
+import React, { useCallback, useState } from 'react';
+import AnimateHeight from 'react-animate-height';
 import { Link } from 'gatsby';
-import React from 'react';
 
-const TimeMachine: React.FC<{ years: string[] }> = ({ years }) => (
-  <nav>
-    <ul>
-      {years.map((year) => (
-        <li key={year}>
-          <Link to={`/${year}`}>{year}</Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+import s from './time-machine.module.css';
+
+const TimeMachine: React.FC<{ years: string[] }> = ({ years }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleToggleOpen = useCallback(() => setOpen((state) => !state), []);
+
+  return (
+    <div className={s.timeMachine}>
+      <button className={s.button} onClick={handleToggleOpen}>
+        Time Machine
+        <svg className={s.chevron} width="10" height="6">
+          <polygon
+            points={open ? '0,6 10,6 5,0' : '0,0 10,0 5,6'}
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+      <AnimateHeight
+        height={open ? 'auto' : 0}
+        duration={200}
+        easing="cubic-bezier(0.645, 0.045, 0.355, 1.000)"
+      >
+        <nav className={s.nav}>
+          <ul>
+            {years.map((year) => (
+              <li key={year} className={s.item}>
+                <Link className={s.link} to={`/${year}`}>
+                  {year}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </AnimateHeight>
+    </div>
+  );
+};
 
 export default TimeMachine;
