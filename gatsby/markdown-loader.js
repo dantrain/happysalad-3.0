fae7d381c7f4829d8cfdf878b4697cf10b637d5a
Dan Train <dan.train@red-badger.com>
2020-10-17 18:23:42 +0100

    Update remark

--- gatsby/markdown-loader.js
+++ gatsby/markdown-loader.js
@@ -2,11 +2,9 @@ const unified = require('unified');
 const remarkParse = require('remark-parse');
 const remarkToRehype = require('remark-rehype');
 
-const processor = unified()
-  .use(remarkParse, { commonmark: true })
-  .use(remarkToRehype);
+const processor = unified().use(remarkParse).use(remarkToRehype);
 
-module.exports = function(source) {
+module.exports = function (source) {
   const output = processor.runSync(processor.parse(source));
 
   return `export default ${JSON.stringify(output)}`;
