7357ec4981af5603c5415699da32ae86cf12e133
Dan Train <dan.train@red-badger.com>
2021-11-15 15:48:21 +0000

    Update Image component to fix broken Giant Bomb CDN urls

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/display-name */
-import React, { useState, useCallback, forwardRef } from 'react';
+import React, { forwardRef, useCallback, useState } from 'react';
 
 type ImageProps = React.ComponentPropsWithRef<'img'>;
 
 const Image = forwardRef<HTMLImageElement, ImageProps>((props, ref) => {
   const [error, setError] = useState(false);
+  const { src, ...rest } = props;
 
   const onError = useCallback(() => {
     setError(true);
@@ -12,7 +13,17 @@ const Image = forwardRef<HTMLImageElement, ImageProps>((props, ref) => {
 
   if (error) return null;
 
-  return <img ref={ref} {...props} onError={onError} />;
+  return (
+    <img
+      ref={ref}
+      src={src.replace(
+        'https://giantbomb1.cbsistatic.com/',
+        'https://www.giantbomb.com/a/'
+      )}
+      {...rest}
+      onError={onError}
+    />
+  );
 });
 
 export default Image;
