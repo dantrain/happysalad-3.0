854dcf4aa0783f90158f2fa85daf8e71bdd7e67d
Dan Train <dan.train@red-badger.com>
2026-02-06 18:31:19 +0000

    Disable Gatsby ESLint webpack plugin (incompatible with ESLint 9)

--- gatsby/webpack-config.js
+++ gatsby/webpack-config.js
@@ -1,6 +1,13 @@
 const path = require('path');
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  // Remove Gatsby's built-in ESLint webpack plugin (incompatible with ESLint 9)
+  const config = getConfig();
+  config.plugins = config.plugins.filter(
+    (plugin) => plugin.constructor.name !== 'ESLintWebpackPlugin'
+  );
+  actions.replaceWebpackConfig(config);
+
   actions.setWebpackConfig({
     module: {
       rules: [
