67d499ae9da65637ffffe606bb2ba5b60d33dcf3
Dan Train <dan.train@red-badger.com>
2026-02-06 18:38:36 +0000

    Fix dev server warnings: add missing subscribeLink CSS, suppress CSS module order warning

--- gatsby/webpack-config.js
+++ gatsby/webpack-config.js
@@ -1,11 +1,20 @@
 const path = require('path');
 
 exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
-  // Remove Gatsby's built-in ESLint webpack plugin (incompatible with ESLint 9)
   const config = getConfig();
-  config.plugins = config.plugins.filter(
-    (plugin) => plugin.constructor.name !== 'ESLintWebpackPlugin'
-  );
+
+  config.plugins = config.plugins.filter((plugin) => {
+    // Remove Gatsby's built-in ESLint webpack plugin (incompatible with ESLint 9)
+    if (plugin.constructor.name === 'ESLintWebpackPlugin') return false;
+
+    // CSS Modules import order doesn't matter — suppress conflicting order warnings
+    if (plugin.constructor.name === 'MiniCssExtractPlugin') {
+      plugin.options.ignoreOrder = true;
+    }
+
+    return true;
+  });
+
   actions.replaceWebpackConfig(config);
 
   actions.setWebpackConfig({
