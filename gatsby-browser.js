5eb6bc897c9d4da8b98e1e9bbec5fc119d9562e5
Dan Train <dan.train@red-badger.com>
2026-02-07 19:47:25 +0000

    Also apply popstate visibility fix on scrollable pages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,27 +8,38 @@ export const wrapPageElement = wrapWithPage;
 
 if (typeof window !== 'undefined') {
   window.addEventListener('popstate', () => {
-    if (window.innerWidth > 768) return;
+    if (
+      window.innerWidth > 768 &&
+      document.documentElement.scrollHeight <= window.innerHeight
+    ) {
+      return;
+    }
+
     const main = document.querySelector('main');
+
     if (main) {
       main.style.visibility = 'hidden';
       if (main.parentElement) main.parentElement.style.background = 'white';
     }
+
     setTimeout(() => {
       const m = document.querySelector('main');
+
       if (m) {
         m.style.visibility = '';
         if (m.parentElement) m.parentElement.style.background = '';
       }
-    }, 200);
+    }, 500);
   });
 }
 
 export const onRouteUpdate = () => {
   const main = document.querySelector('main');
+
   if (main) {
     main.style.visibility = '';
     if (main.parentElement) main.parentElement.style.background = '';
   }
+
   setTimeout(() => store.dispatch(close()), 100);
 };
