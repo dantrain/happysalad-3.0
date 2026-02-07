339d1ccad292b3ccf2aa1817773bdee8ed59a53d
Dan Train <dan.train@red-badger.com>
2026-02-07 18:21:48 +0000

    Hide main content during popstate to prevent iOS Safari back-nav flash

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,28 @@ import { close } from './src/features/mobileMenu/mobileMenuSlice';
 export const wrapRootElement = wrapWithProvider;
 export const wrapPageElement = wrapWithPage;
 
+if (typeof window !== 'undefined') {
+  window.addEventListener('popstate', () => {
+    const main = document.querySelector('main');
+    if (main) {
+      main.style.visibility = 'hidden';
+      if (main.parentElement) main.parentElement.style.background = 'white';
+    }
+    setTimeout(() => {
+      const m = document.querySelector('main');
+      if (m) {
+        m.style.visibility = '';
+        if (m.parentElement) m.parentElement.style.background = '';
+      }
+    }, 200);
+  });
+}
+
 export const onRouteUpdate = () => {
+  const main = document.querySelector('main');
+  if (main) {
+    main.style.visibility = '';
+    if (main.parentElement) main.parentElement.style.background = '';
+  }
   setTimeout(() => store.dispatch(close()), 100);
 };
