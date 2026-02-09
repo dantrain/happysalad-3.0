29a071d11fdade71d34cdb1c75b2699920b0592b
Dan Train <dan.train@red-badger.com>
2026-02-09 19:02:24 +0000

    Virtualize podcast posts list on game page

--- src/templates/GamePage/GamePage.tsx
+++ src/templates/GamePage/GamePage.tsx
@@ -1,16 +1,104 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { graphql } from 'gatsby';
 import { decode } from 'he';
+import { useWindowVirtualizer } from '@tanstack/react-virtual';
 import VideoPost from '../../components/VideoPost/VideoPost';
 import PodcastPost from '../../components/PodcastPost/PodcastPost';
 import Image from '../../components/Image/Image';
 import SEO from '../../components/SEO';
+import heightCache, { scrollStateCache } from '../../utils/heightCache';
 
 import * as s from './game-page.module.css';
 
 type GamePageProps = {
   pageContext: { name: string; summary: string; image: { cover: string } };
   data: Queries.PostsBySlugsQuery;
+};
+
+type PodcastEdge =
+  Queries.PostsBySlugsQuery['allContentfulPodcastPost']['edges'][number];
+
+const VirtualizedPodcastList: React.FC<{
+  podcastPosts: readonly PodcastEdge[];
+}> = ({ podcastPosts }) => {
+  const listRef = useRef<HTMLDivElement>(null);
+  const scrollMarginRef = useRef(0);
+
+  useLayoutEffect(() => {
+    scrollMarginRef.current = listRef.current?.offsetTop ?? 0;
+  }, []);
+
+  const itemsRef = useRef(podcastPosts);
+  itemsRef.current = podcastPosts;
+
+  const cacheKey =
+    typeof window !== 'undefined' ? window.location.pathname : '/';
+  const savedState = scrollStateCache.get(cacheKey);
+
+  const virtualizer = useWindowVirtualizer({
+    count: podcastPosts.length,
+    estimateSize: (index) => {
+      const slug = itemsRef.current[index]?.node.slug;
+      if (slug && heightCache.has(slug)) return heightCache.get(slug)!;
+
+      return 350;
+    },
+    overscan: 8,
+    scrollMargin: scrollMarginRef.current,
+    measureElement: (el) => {
+      const height = el.getBoundingClientRect().height;
+      const index = Number((el as HTMLElement).dataset.index);
+      const slug = itemsRef.current[index]?.node.slug;
+      if (slug) heightCache.set(slug, height);
+
+      return height;
+    },
+    initialOffset: savedState?.offset,
+    initialMeasurementsCache: savedState?.measurementsCache,
+    scrollToFn: () => {},
+    onChange: (instance) => {
+      if (!instance.isScrolling) {
+        scrollStateCache.set(cacheKey, {
+          offset: instance.scrollOffset ?? 0,
+          measurementsCache: instance.measurementsCache,
+        });
+      }
+    },
+  });
+
+  const virtualItems = virtualizer.getVirtualItems();
+
+  return (
+    <div ref={listRef}>
+      <ul
+        style={{
+          height: virtualizer.getTotalSize(),
+          position: 'relative',
+        }}
+      >
+        {virtualItems.map((virtualItem) => {
+          const edge = podcastPosts[virtualItem.index];
+
+          return (
+            <li
+              key={edge.node.slug}
+              data-index={virtualItem.index}
+              ref={virtualizer.measureElement}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                transform: `translateY(${virtualItem.start - virtualizer.options.scrollMargin}px)`,
+              }}
+            >
+              <PodcastPost {...edge.node} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 const GamePage: React.FC<GamePageProps> = ({
@@ -25,6 +113,9 @@ const GamePage: React.FC<GamePageProps> = ({
   },
 }) => {
   const description = summary && summary !== name && decode(summary);
+
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => setIsClient(true), []);
 
   return (
     <>
@@ -43,13 +134,17 @@ const GamePage: React.FC<GamePageProps> = ({
           </li>
         ))}
       </ul>
-      <ul>
-        {podcastPosts.map(({ node }) => (
-          <li key={node.slug}>
-            <PodcastPost {...node} />
-          </li>
-        ))}
-      </ul>
+      {isClient ? (
+        <VirtualizedPodcastList podcastPosts={podcastPosts} />
+      ) : (
+        <ul>
+          {podcastPosts.map(({ node }) => (
+            <li key={node.slug}>
+              <PodcastPost {...node} />
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
